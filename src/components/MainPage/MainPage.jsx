6323b6397f0041d44e8d9f5ef7000123091ab62a
Matias Pierobon <matias.pierobonm@gmail.com>
2017-05-15 17:32:29 -0300

    add main page

---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import List from 'react-md/lib/Lists/List';
+import ListItem from 'react-md/lib/Lists/ListItem';
+import FontIcon from 'react-md/lib/FontIcons';
+
+const HomeIcon = () => <FontIcon>home</FontIcon>;
+const ExplorIcon = () => <FontIcon>explor</FontIcon>;
+const ProfileIcon = () => <FontIcon>profile</FontIcon>;
+
+
+const SideBar = () => (<aside>
+        <List>
+      <ListItem leftIcon={<HomeIcon />} primaryText="Inicio" />
+      <ListItem leftIcon={<ExplorIcon />} primaryText="Explorar" />
+      <ListItem leftIcon={<ProfileIcon />} primaryText="Mi Perfil" />
+    </List>
+</aside>)
+
+const MainPage = ({ children }) => (
+    <main>
+        <SideBar />
+        <content>
+            { children }
+        </content>
+    </main>
+)
