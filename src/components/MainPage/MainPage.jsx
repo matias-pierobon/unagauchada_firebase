01b6650439c94ba47a353f6aaba32bd0bb11c7d2
Matias Pierobon <matias.pierobonm@gmail.com>
2017-05-15 17:33:06 -0300

    format MainPage

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import List from 'react-md/lib/Lists/List';
-import ListItem from 'react-md/lib/Lists/ListItem';
-import FontIcon from 'react-md/lib/FontIcons';
+import React from "react"
+import List from "react-md/lib/Lists/List"
+import ListItem from "react-md/lib/Lists/ListItem"
+import FontIcon from "react-md/lib/FontIcons"
 
-const HomeIcon = () => <FontIcon>home</FontIcon>;
-const ExplorIcon = () => <FontIcon>explor</FontIcon>;
-const ProfileIcon = () => <FontIcon>profile</FontIcon>;
+const HomeIcon = () => <FontIcon>home</FontIcon>
+const ExplorIcon = () => <FontIcon>explor</FontIcon>
+const ProfileIcon = () => <FontIcon>profile</FontIcon>
 
-
-const SideBar = () => (<aside>
-        <List>
+const SideBar = () => (
+  <aside>
+    <List>
       <ListItem leftIcon={<HomeIcon />} primaryText="Inicio" />
       <ListItem leftIcon={<ExplorIcon />} primaryText="Explorar" />
       <ListItem leftIcon={<ProfileIcon />} primaryText="Mi Perfil" />
     </List>
-</aside>)
+  </aside>
+)
 
 const MainPage = ({ children }) => (
-    <main>
-        <SideBar />
-        <content>
-            { children }
-        </content>
-    </main>
+  <main>
+    <SideBar />
+    <content>
+      {children}
+    </content>
+  </main>
 )
